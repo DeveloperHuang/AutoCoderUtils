09a5a0e494fc0a62eea637cc9f73a02f26cdb556
HuangGongJue <511867317@qq.com>
2016-10-11 18:03:12 +0800

    Mapper界面控件布置及功能实现

---
+++ src/com/huang/auto/coder/utils/DialogMessageUtils.java
@@ -0,0 +1,35 @@
+package com.huang.auto.coder.utils;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by JianQiu on 2016/10/11.
+ */
+public class DialogMessageUtils {
+
+    public static final long DEFAULT_CLOSED_TIME = 2000;
+
+    public static void sendMessage(JComponent component ,String message){
+        sendMessage(component,message,DEFAULT_CLOSED_TIME);
+    }
+
+    public static void sendMessage(JComponent component ,String message,long closeTime){
+        JDialog dialog = new JDialog();
+        dialog.setLocationRelativeTo(component);
+        dialog.add(new JLabel(message));
+        dialog.setSize(100,60);
+        dialog.setVisible(true);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(closeTime);
+                    dialog.setVisible(false);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
